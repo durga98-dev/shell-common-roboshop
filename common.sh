445d4f442d31e89cbb53a1a24da8cb140d96a877
Durga <durgadevi0707@gmail.com>
2026-01-07 00:38:49 +0530

    common shell script

--- common.sh
+++ common.sh
@@ -10,15 +10,19 @@ N="\e[0m"
 LOG_FOLDER="/var/log/shell-script"
 LOG_FILE_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOG_FOLDER/$LOG_FILE_NAME.log"
+SCRIPT_DIR=$PWD
+START_TIME=$(date +%s)
+MONGODB_HOST=mongodb.durgadevops.fun
 
 mkdir -p $LOG_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-if [ $USERID -ne 0 ]; then
-    echo -e "$R ERROR:: Run the script with root privilege $N"
-    exit 1
-fi
-
+checkroot(){
+    if [ $USERID -ne 0 ]; then
+        echo -e "$R ERROR:: Run the script with root privilege $N"
+        exit 1
+    fi
+}
 # Function - will not run unless until we call it explicitly
 VALIDATE(){
     if [ $1 -ne 0 ]; then
@@ -27,4 +31,60 @@ VALIDATE(){
     else
         echo -e "$2 $G SUCCESS $N" | tee -a $LOG_FILE
     fi
+}
+
+nodejs_setup(){
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Disabling module" 
+
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "Enabling required module"
+
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "Installing Nodejs"
+
+    npm install &>>$LOG_FILE
+    VALIDATE $? "Install dependencies" 
+}
+
+app_setup(){
+    mkdir -p /app 
+    VALIDATE $? "creating app directory"
+
+    curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip 
+    VALIDATE $? "Downloading $app_name application"
+
+    cd /app 
+    VALIDATE $? "Change the directory"
+
+    rm -rf /app/*
+    VALIDATE $? "Removing the existing code"
+
+    unzip /tmp/$app_name.zip &>>$LOG_FILE
+    VALIDATE $? "Unzip $app_name application"
+}
+
+systemd_setup(){
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
+    VALIDATE $? "copy the service file"
+
+    systemctl daemon-reload
+    VALIDATE $? "Reload the service"
+
+    systemctl enable $app_name &>>$LOG_FILE
+    VALIDATE $? "Enable the service"
+
+    systemctl start $app_name &>>$LOG_FILE
+    VALIDATE $? "Start the service"
+}
+
+app_restart(){
+    systemctl restart $app_name
+    VALIDATE $? "Restarted $app_name"
+}
+
+print_total_time(){
+    END_TIME=$(date +%s)
+    TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+    echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
 }
--- mongodb.sh
+++ mongodb.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 source ./common.sh
+
+checkroot
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding Mongo repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? "Installing Mongo DB"
+
+systemctl enable mongod 
+VALIDATE $? "Enabling Mongo DB"
+
+systemctl start mongod 
+VALIDATE $? "Started Mongo DB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod
+VALIDATE $? "Restarted the MongoDB"
+
+print_total_time
