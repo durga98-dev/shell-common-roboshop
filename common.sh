9c33bba66c81262dbd012428d3ac77d20d705ae6
Durga <durgadevi0707@gmail.com>
2026-01-07 20:49:32 +0530

    common shell script

--- common.sh
+++ common.sh
@@ -97,6 +97,14 @@ java_setup(){
     VALIDATE $? "Renaming the artifact"
 }
 
+python_setup(){
+    dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+    VALIDATE $? "Installing Python"
+
+    pip3 install -r requirements.txt &>>$LOG_FILE
+    VALIDATE $? "Installing dependencies"
+}
+
 app_restart(){
     systemctl restart $app_name
     VALIDATE $? "Restarted $app_name"
