1cb9ede271fccf18c7114d2215f93b3a31cf422f
Durga <durgadevi0707@gmail.com>
2026-01-07 20:02:05 +0530

    common shell script

--- mongodb.sh
+++ mongodb.sh
@@ -7,19 +7,19 @@ checkroot
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Adding Mongo repo"
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "Installing Mongo DB"
 
-systemctl enable mongod 
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enabling Mongo DB"
 
-systemctl start mongod 
+systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Started Mongo DB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections to MongoDB"
 
-systemctl restart mongod
+systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "Restarted the MongoDB"
 
 print_total_time
